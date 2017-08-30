325bf8449986042d675156fd9d868685627de56b
Daniel Klabbers <daniel@productip.com>
2017-08-30 15:56:35 +0200

    okay, lets just try staging instead

--- tests/unit-tests/Webserver/Certificate/LetsEncryptGeneratorTest.php
+++ tests/unit-tests/Webserver/Certificate/LetsEncryptGeneratorTest.php
@@ -27,8 +27,8 @@ class LetsEncryptGeneratorTest extends Test
 
     protected function duringSetUp(Application $app)
     {
-        $app['config']->set('webserver.lets-encrypt.directory-url', 'http://boulder:4000');
-        $app['config']->set('webserver.lets-encrypt.agreement-url', 'http://boulder:4000/terms/v1');
+        $app['config']->set('webserver.lets-encrypt.directory-url', 'https://acme-staging.api.letsencrypt.org/directory');
+        $app['config']->set('webserver.lets-encrypt.agreement-url', 'https://acme-staging.api.letsencrypt.org/terms');
         $app['config']->set('webserver.lets-encrypt.key-pair.private', __DIR__ . '/../../../../ci-lets-encrypt-private.pem');
         $app['config']->set('webserver.lets-encrypt.key-pair.public', __DIR__ . '/../../../../ci-lets-encrypt-public.pem');
 
